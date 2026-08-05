ef2108af38b6662509504741ac45356e9eb3ecaf
Chr-H <christian.holmer@online.de>
2026-08-05 04:17:25 +0200

    Service Worker Auto-Update & Cache-Buster v0.5.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'ffw-manager-v1';
+const CACHE_NAME = 'ffw-manager-v0.5.2'; // <-- Bei künftigen Updates hier einfach v0.5.3 etc. eintragen
+
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -6,21 +7,38 @@ const ASSETS_TO_CACHE = [
   './js/storage.js',
   './js/kategorien.js',
   './js/geraete.js',
+  './js/fahrzeuge.js',
   './js/app.js',
   './manifest.json'
 ];
 
-// Service Worker installieren & Dateien cachen
+// Install Event: Neuen Cache anlegen
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Erzwingt das sofortige Aktivieren des neuen Service Workers
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching aller Dateien...');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// Anfragen abfangen & Offline-Daten liefern
+// Activate Event: Alte Caches automatisch löschen
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Lösche alten Cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Übernimmt sofort die Kontrolle
+  );
+});
+
+// Fetch Event: Dateiaufrufe bedienen
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
