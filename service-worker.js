932e1ba86a176ddc87339f21e387b905d11d3028
Chr-H <christian.holmer@online.de>
2026-08-18 18:23:12 +0200

    v4.7.0

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 // ==========================================
-// FFW Manager - Service Worker (v4.6.9.)
+// FFW Manager - Service Worker (v4.7.0.)
 // ==========================================
 
-const CACHE_NAME = 'ffw-manager-v4.6.9'; // Version erhöht für Icon-Caching
+const CACHE_NAME = 'ffw-manager-v4.7.0'; // Version erhöht für Icon-Caching
 
 const ASSETS_TO_CACHE = [
   './',
