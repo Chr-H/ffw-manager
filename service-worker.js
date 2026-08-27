24a9f81dc0307e648c015cda3359d61c7ce7b6ec
Chr-H <christian.holmer@online.de>
2026-08-27 05:08:02 +0200

    Revert "v5.6.9"
    This reverts commit 552e7b04ad28a35edb81113a1ea679dd040c4ec7.

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 // ==========================================
-// FFW Manager - Service Worker (v5.6.9.)
+// FFW Manager - Service Worker (v5.6.8.)
 // ==========================================
 
-const CACHE_NAME = 'ffw-manager-v5.6.9'; // Version erhöht für Icon-Caching
+const CACHE_NAME = 'ffw-manager-v5.6.8'; // Version erhöht für Icon-Caching
 
 const ASSETS_TO_CACHE = [
   './',
